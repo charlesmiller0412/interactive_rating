a0036bf294a57d60a12d2e0762ede19d685b6f91
Charles Miller <charles.miller.0412@gmail.com>
2023-01-03 22:22:54 -0600

    added different message for low response

--- src/components/thanks.tsx
+++ src/components/thanks.tsx
@@ -9,8 +9,9 @@ export const Thanks = (props: any) => {
             <span>You selected {props.choice} out of 5</span>
             <h2>Thank you!</h2>
             <p>
-                We appreciate you taking the time to give a rating. If you ever
-                need more support, don’t hesitate to get in touch!
+                {props.choice > 3
+                    ? "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!"
+                    : "We appreciate you taking the time to give a rating. Sorry we didn't meet your expectations. Feel free to email us with further suggestions!"}
             </p>
         </div>
     );
